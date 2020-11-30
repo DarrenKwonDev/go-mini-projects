ff958d0eada019d1c05162e7e63a8a8f1e7bfd98
DarrenKwonDev <anatomy1545@gmail.com>
2020-12-01 03:12:17 +0900

    delete nodemon use fresh

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/DarrenKwonDev/learnGo/accounts"
-)
+import "fmt"
 
 func main() {
-	account := accounts.NewAccount("darren")
-	// account.Deposit(10)
-	// fmt.Println(account.Balance())
-	// account.NewOwner("lee")
-	// fmt.Println(account)
-	// fmt.Println(account.Owner())
-	fmt.Println(account)
-	// err := account.Withraw(20)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
+	fmt.Println("wass")
 }

94c88a6abc43f8a975cef53379349549025997e1
DarrenKwonDev <anatomy1545@gmail.com>
2020-12-01 04:17:05 +0900

    map to dict

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	mydict "github.com/DarrenKwonDev/learnGo/dict"
+)
 
 func main() {
-	fmt.Println("wass")
+	dictionary := mydict.Dictionary{"first": "word"}
+	definition, err := dictionary.Search("first")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
+	dictionary.Add("second", "whwhwh")
+	fmt.Println(dictionary)
+	dictionary.Delete("first")
+	fmt.Println(dictionary)
 }

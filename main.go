ec6c1ebc3b4d756fd2c865d3326530dd87ad06f4
DarrenKwonDev <anatomy1545@gmail.com>
2020-12-01 17:51:03 +0900

    slow url checker

--- main.go
+++ main.go
@@ -1,21 +1,48 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	mydict "github.com/DarrenKwonDev/learnGo/dict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request failed")
+
 func main() {
-	dictionary := mydict.Dictionary{"first": "word"}
-	definition, err := dictionary.Search("first")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+
+	var results = make(map[string]string)
+
+	urls := []string{"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://cineps.net/"}
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
 	}
-	dictionary.Add("second", "whwhwh")
-	fmt.Println(dictionary)
-	dictionary.Delete("first")
-	fmt.Println(dictionary)
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking url" + url)
+	resp, err := http.Get(url)
+
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err)
+		fmt.Println(resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
 }
